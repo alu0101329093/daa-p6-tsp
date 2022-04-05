5b56e31dadc519803af9ab3dde18bcf2be1b5f8e
Jaime <alu0101329093@ull.edu.es>
2022-04-05 07:48:40 +0100

    Added brute force algorithm

---
+++ src/tsp_brute_force.cpp
@@ -0,0 +1,40 @@
+#include "tsp_algorithms/tsp_brute_force.h"
+
+namespace daa {
+
+TspSolution TspBruteForce::Execute(
+    const std::set<std::string>& nodes_list,
+    const std::map<TspPath, std::size_t>& path_costs) {
+  const auto start_time = std::chrono::steady_clock::now();
+  std::size_t min_cost{INT_MAX};
+  std::vector<std::string> min_path{};
+  std::vector<std::string> nodes(nodes_list.begin(), nodes_list.end());
+  std::vector<std::string> search_nodes(nodes.begin() + 1, nodes.end());
+  // nodes.pop_back();
+  do {
+    auto actual_time = std::chrono::steady_clock::now();
+    auto execution_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+        actual_time - start_time);
+    if (execution_time >= kAlgotithmMaxExecutionTime)
+      return TspSolution{min_path, execution_time, min_cost};
+    std::string actual_node{nodes[0]};
+    std::size_t actual_cost{0};
+    std::vector<std::string> actual_path{actual_node};
+    for (const auto& node : search_nodes) {
+      actual_cost += path_costs.at(TspPath{actual_node, node});
+      actual_node = node;
+      actual_path.push_back(node);
+    }
+    actual_cost += path_costs.at(TspPath{actual_node, nodes[0]});
+    if (actual_cost < min_cost) {
+      min_cost = actual_cost;
+      min_path = actual_path;
+    }
+  } while (std::next_permutation(search_nodes.begin(), search_nodes.end()));
+  auto actual_time = std::chrono::steady_clock::now();
+  auto execution_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+      actual_time - start_time);
+  return TspSolution{min_path, execution_time, min_cost};
+}
+
+}  // namespace daa

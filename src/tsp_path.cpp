22db5112d29e35f83e04016c098ccf12b8f35914
Jaime <alu0101329093@ull.edu.es>
2022-04-05 07:48:27 +0100

    Updated path to get smaller node first

--- src/tsp_path.cpp
+++ src/tsp_path.cpp
@@ -3,6 +3,7 @@
 namespace daa {
 
 TspPath::TspPath(const std::string& first_node, const std::string& second_node)
-    : first_node_{first_node}, second_node_{second_node} {}
+    : first_node_{first_node < second_node ? first_node : second_node},
+      second_node_{first_node < second_node ? second_node : first_node} {}
 
 }  // namespace daa
